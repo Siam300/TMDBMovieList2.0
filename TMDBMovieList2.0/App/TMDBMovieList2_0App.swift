7296dbe6512790aa9c828be45d921aad801fbd02
Siam300 <89306483+Siam300@users.noreply.github.com>
2023-12-29 15:06:24 +0600

    TMDBMovielist2.0 project created

--- TMDBMovieList2.0/App/TMDBMovieList2_0App.swift
+++ TMDBMovieList2.0/App/TMDBMovieList2_0App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TMDBMovieList2_0App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MovieListView()
         }
     }
 }
--- TMDBMovieList2.0/Core/All Movies/View/MovieListView.swift
+++ TMDBMovieList2.0/Core/All Movies/View/MovieListView.swift
@@ -7,18 +7,18 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MovieListView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("MovieListView")
         }
         .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    MovieListView()
 }
