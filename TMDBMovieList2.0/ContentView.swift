3bc8da7f7e3c501c0fe9bbd44e90ab79fd369fc8
Siam300 <89306483+Siam300@users.noreply.github.com>
2023-12-29 15:00:24 +0600

    Initial Commit

---
+++ TMDBMovieList2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TMDBMovieList2.0
+//
+//  Created by Auto on 29/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TMDBMovieList2.0/TMDBMovieList2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  TMDBMovieList2_0App.swift
+//  TMDBMovieList2.0
+//
+//  Created by Auto on 29/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TMDBMovieList2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
